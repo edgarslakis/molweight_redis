73e38f94e8e9f29ef020734fb9aac3e1fca51efb
Edgars Lakis <edgars_lakis@inbox.lv>
2023-10-25 12:26:47 +0000

    Style and clear up the code

--- main.py
+++ main.py
@@ -10,32 +10,33 @@ r = redis.Redis(
 
 
 # Datu bāzē ievieto vismaz 500 ierakstus no CSV faila
-with open("dataset.csv", "r") as file:
+with open("dataset.csv", "r", encoding="utf-8") as file:
+    print(file)
     reader = csv.reader(file, delimiter=";")
     for line in reader:
         r.set(line[0],line[1])
 
 # Maina 20 ierakstus
-with open("dataset.csv", "r") as file:
-        reader = csv.reader(file, delimiter=";")
-        interesting_rows = [row for idx, row in enumerate(reader) if idx < 21]
-        for row in interesting_rows:
-            r.set(row[0],row[2])
+with open("dataset.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file, delimiter=";")
+    interesting_rows = [row for idx, row in enumerate(reader) if idx < 21]
+    for row in interesting_rows:
+        r.set(row[0],row[2])
 
 # Atlasa 20 ierakstus
-with open("dataset.csv", "r") as file:
-        reader = csv.reader(file, delimiter=";")
-        cool_rows = [row[0] for idx, row in enumerate(reader) if (idx > 100) & (idx < 121)]
-        for row in cool_rows:
-            r.get(row)
+with open("dataset.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file, delimiter=";")
+    cool_rows = [row[0] for idx, row in enumerate(reader) if (idx > 100) & (idx < 121)]
+    for row in cool_rows:
+        r.get(row)
 
 print("Datubāzes izmērs pirms 20 ierakstu dzēšanas: ", r.dbsize())
 # Dzēš 20 ierakstus
-with open("dataset.csv", "r") as file:
-        reader = csv.reader(file, delimiter=";")
-        boring_rows = [row[0] for idx, row in enumerate(reader) if (idx > 200) & (idx < 221)]
-        for row in boring_rows:
-            r.delete(row)
+with open("dataset.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file, delimiter=";")
+    boring_rows = [row[0] for idx, row in enumerate(reader) if (idx > 200) & (idx < 221)]
+    for row in boring_rows:
+        r.delete(row)
 print("Datubāzes izmērs pēc 20 ierakstu dzēšanas: ", r.dbsize())
 
 print("Atslēgas, kuras satur patern 'CU...': ", r.keys("CU*"))
